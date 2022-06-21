cd6132678d33c71c32bb8b3ba703d8a7e01ea420
aantia <agjantia@gmail.com>
2022-06-21 17:29:56 +0100

    and another in drawdown lines incremented version number

--- quantstats/_plotting/wrappers_n.py
+++ quantstats/_plotting/wrappers_n.py
@@ -123,10 +123,11 @@ def snapshot(returns, grayscale=False, figsize=(10, 8),
     axes[1].set_ylabel('Drawdown', fontname=fontname,
                        fontweight='bold', fontsize=12)
     axes[1].set_yticks(_np.arange(-ddmin, 0, step=ddmin_ticks))
-    axes[1].plot(dd, color=colors[2], lw=1 if grayscale else lw, zorder=1)
     axes[1].axhline(0, color='silver', lw=1, zorder=0)
-    if not grayscale:
-        for line in dd:
+    for line in dd:
+        axes[1].plot(dd, color=colors[2], lw=1 if grayscale else lw, zorder=1)
+        if not grayscale:
+            print(line)
             axes[1].fill_between(line.index, 0, dd, color=colors[2], alpha=.1)
 
     axes[1].set_yscale("symlog" if log_scale else "linear")
