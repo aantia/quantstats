44c77511028ee10ce8439447120e3fbbdb0ef59f
aantia <agjantia@gmail.com>
2022-06-28 12:43:43 +0100

    experimenting with legend location increment version

--- quantstats/_plotting/wrappers_n.py
+++ quantstats/_plotting/wrappers_n.py
@@ -167,7 +167,7 @@ def snapshot(returns, grayscale=False, figsize=(10, 8),
         ax.set_facecolor('white')
         ax.yaxis.set_label_coords(-.1, .5)
         ax.yaxis.set_major_formatter(_StrMethodFormatter('{x:,.0f}%'))
-        ax.legend(fontsize=10, loc='lower center', bbox_to_anchor=(0, -0.1, 1, 1), ncol=3, bbox_transform=_plt.gcf().transFigure)
+        #ax.legend(fontsize=10, loc='lower center', bbox_to_anchor=(0, -0.1, 1, 1), ncol=3, bbox_transform=_plt.gcf().transFigure)
         #ax.legend(fontsize=10, loc='center left', bbox_to_anchor=(1, 0.5), ncol=3)
 
     _plt.subplots_adjust(hspace=0, bottom=0, top=1)
@@ -180,7 +180,7 @@ def snapshot(returns, grayscale=False, figsize=(10, 8),
     #_plt.legend(lines, leg, loc='lower center', bbox_to_anchor=(0, -0.1, 1, 1), bbox_transform=_plt.gcf().transFigure)
     leg = {}
     for ax in axes:
-        leg[str(ax)] = _utils.InteractiveLegend(legend=ax.legend())
+        leg[str(ax)] = _utils.InteractiveLegend(legend=ax.legend(fontsize=10, loc='lower center', bbox_to_anchor=(0, -0.1, 1, 1), ncol=3, bbox_transform=_plt.gcf().transFigure))
 
     try:
         _plt.subplots_adjust(hspace=0)
